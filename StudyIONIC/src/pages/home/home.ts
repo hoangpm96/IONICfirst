9f85a03c6aa85b85a0deb17ff78c23f4d2b5a5a2
hoangpm96 <hoangpm.qn96@gmail.com>
2017-02-27 22:04:27 +0700

    add button

--- StudyIONIC/src/pages/home/home.ts
+++ StudyIONIC/src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
-
+import {ContactPage} from "../contact/contact";
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -11,5 +11,7 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+    onGotoContact() {
+    this.navCtrl.push(ContactPage);
+}
 }
